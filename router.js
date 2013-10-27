8c6f5005590c16ac0d9b83f75987f5c4d6989add
Flarnie Marchan <fnonemaker@gmail.com>
2013-10-26 20:43:22 -0700

    Fix antipattern of checking for file existence before loading.

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ var serveFile = function(response, absPath){
 	fs.readFile(absPath, function(err, data) {
 		if (err) {
 			console.log("ERROR LOGGED: ", err);
-			serveError(response, 500);
+			serveError(response, 404);
 		} else {
 			response.writeHead(
 				200,
@@ -23,8 +23,6 @@ var serveError = function(response, errorCode){
   var message;
   if (errorCode === 404){
     message = 'Error 404: resource not found.';
-  } else if (errorCode === 500){
-    message = 'Error 500: there was a problem serving the requested file.';
   } else {
     message = 'Error: there was a problem.';    
   }
@@ -39,14 +37,8 @@ var router = function(request, response){
 	if (url === "/"){
 		serveFile(response, "public/index.html");
 	} else {
-	  fs.exists((url), function(err, data){
-			if (err) {
-				serveError(response, 404);
-			} else {
-				serveFile(response, ("public" + url));
-			} // end inner if/else
-	  }); // end outer fs.exists callback
+	  serveFile(response, ("public" + url));
 	} // end outer if/else
 } // end router function
 
-export.router = router;
+exports.router = router;
