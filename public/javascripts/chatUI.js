afae145f2d3ae2d214ea8e184493cfb0ed4bc013
PKLong <patrickklong@gmail.com>
2017-08-07 17:28:40 -0400

    WIP

--- public/javascripts/chatUI.js
+++ public/javascripts/chatUI.js
@@ -3,18 +3,23 @@ const Chat = require('./chat')
 function ChatUI (socket) {
   this.chat = new Chat(socket)
   this.form = document.querySelector('form')
-  this.msgList = document.querySelector('ul')
+  this.msgList = document.querySelector('ul#msg-list')
+  this.roomList = document.querySelector('ul#room-list')
   this.input = document.querySelector('input')
+  this.room = document.querySelector('#room')
   this.submitHandler()
-  this.messageHandler()
 }
 
 ChatUI.prototype.getInput = function () {
   return this.input.value
 }
 
-ChatUI.prototype.sendMsg = function () {
-  this.chat.sendMessage(this.getInput())
+ChatUI.prototype.setRoom = function (room) {
+  this.room.textContent = room
+}
+
+ChatUI.prototype.sendMsg = function (room) {
+  this.chat.sendMessage(room, this.getInput())
 }
 
 ChatUI.prototype.addMsg = function (msg) {
@@ -23,18 +28,34 @@ ChatUI.prototype.addMsg = function (msg) {
   this.msgList.appendChild(newMessage)
 }
 
+ChatUI.prototype.addRoom = function(room) {
+  const newRoom = document.createElement('li')
+  newRoom.textContent = room
+  this.roomList.appendChild(newRoom)
+  
+}
+
 ChatUI.prototype.submitHandler = function () {
   this.form.addEventListener('submit', (e) => {
     e.preventDefault()
-    this.sendMsg()
+    this.processUserInput()
     this.input.value = ''
   })
 }
 
-ChatUI.prototype.messageHandler = function () {
-  this.chat.socket.on('message', (msg) => {
+ChatUI.prototype.processUserInput = function () {
+  const msg = this.getInput()
+  let response
+  if (msg[0] === '/') {
+    response = this.chat.processCommand(msg)
+    if (response) {
+      this.addMsg(response)
+    }
+  } else {
+    this.sendMsg(this.room.textContent)
     this.addMsg(msg)
-  })
+  }
 }
+
 
 module.exports = ChatUI
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,4 +2,41 @@ document.addEventListener('DOMContentLoaded', () => {
   const socket = require('socket.io-client')()
   const ChatUI = require('./chatUI')
   const myChat = new ChatUI(socket)
+  socket.on('nameResult', (result) => {
+    let msg
+    if (result.success) {
+      msg = `Name changed to ${result.name}.`
+    } else {
+      msg = result.message
+    }
+    myChat.addMsg(msg)
+  })
+  
+  socket.on('joinResult', (result) => {
+    myChat.setRoom(result.room)
+    myChat.addMsg('Room changed.')
+  })
+  
+  socket.on('message', (message) => {
+    myChat.addMsg(message.text)
+  })
+  
+  socket.on('rooms', (rooms) => {
+    myChat.roomList.innerHTML = ''
+    console.log(rooms)
+    rooms.forEach(room => myChat.addRoom(room))
+    myChat.roomList.querySelectorAll('li').forEach( li => {
+      li.addEventListener('click', (e) => {
+	myChat.chat.processCommand(`/join ${li.textContent}`)
+	myChat.input.focus()
+      })
+    })
+  })
+
+  setInterval(() => {
+    socket.emit('rooms')
+  }, 1000)
+
+  myChat.input.focus()
+  
 })
